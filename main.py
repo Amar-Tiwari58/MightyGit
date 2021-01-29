eda64cc6b7cafcd4a9b718a97b197c28e3ada0b1
Amar Tiwari <=>
2021-01-29 16:08:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pywhatkit
+import pyttsx3
+import speech_recognition as sr
+listner = sr.Microphone(pywhatkit)
+print()
+pywhatkit.sendwhatmsg("+9199877670108","Hacked 🤷‍♀️",16,9)
+print(pywhatkit)
